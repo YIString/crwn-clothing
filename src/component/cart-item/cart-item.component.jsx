8fb7b3292806e73c846ee8e3d0a56319d772a0c4
chen <victor.songpengyu@icloud.com>
2022-10-01 23:34:24 +0800

    added in our addToCart feature

--- src/component/cart-item/cart-item.component.jsx
+++ src/component/cart-item/cart-item.component.jsx
@@ -1,10 +1,17 @@
 import './cart-item.style.scss'
 
-const CartItem = () => {
+const CartItem = ({ cartItem }) => {
+  const { name, quantity, imageUrl, price } = cartItem
 
   return (
-    <div>
-      <img src="" alt="" />
+    <div className="cart-item-container">
+      <img src={imageUrl} alt={`${name}`} />
+      <div className="item-details">
+        <span className="name">{name}</span>
+        <span>
+          {quantity} x {`$${price}`}
+        </span>
+      </div>
     </div>
   )
 }
--- src/context/cart.context.jsx
+++ src/context/cart.context.jsx
@@ -1,9 +1,51 @@
-import { createContext, useState } from 'react'
+import { createContext, useState, useEffect } from 'react'
 
-export const CartContext = createContext()
+const addCartItem = (cartItem, productToAdd) => {
+  //find if cartItems contains productToAdd
+  const existingCartItem = cartItem.find(
+    (cartItem) => cartItem.id === productToAdd.id
+  )
+  //if found increment quantity
+  if (existingCartItem) {
+    return cartItem.map((cartItem) =>
+      cartItem.id === productToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    )
+  }
+  //return new array with modified cartItems / new cart items
+  return [...cartItem, { ...productToAdd, quantity: 1 }]
+}
+export const CartContext = createContext({
+  isCartOpen: false,
+  setIsCartOpen: () => {},
+  cartItem: [],
+  addItemToCart: () => {},
+  cartCount: 0,
+})
 
 export const CartProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false)
-  const value = { isCartOpen, setIsCartOpen }
+  const [cartItem, setCartItem] = useState([])
+  const [cartCount, setCartCount] = useState(0)
+  useEffect(() => {
+    const newCartCount = cartItem.reduce(
+      (total, cartItem) => total + cartItem.quantity,
+      0
+    )
+    setCartCount(newCartCount)
+  }, [cartItem])
+
+  const addItemToCart = (productToAdd) => {
+    setCartItem(addCartItem(cartItem, productToAdd))
+  }
+
+  const value = {
+    isCartOpen,
+    setIsCartOpen,
+    addItemToCart,
+    cartItem,
+    cartCount,
+  }
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>
 }
